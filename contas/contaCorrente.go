88305b3d0b4d8b3fc9ff9574332ee5338ef8fd62
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-01 13:10:54 -0300

    POO: pacotes e visibilidade

---
+++ contas/contaCorrente.go
@@ -0,0 +1,35 @@
+package contas
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque <= c.Saldo && valorDoSaque > 0
+	if podeSacar {
+		c.Saldo -= valorDoSaque
+		return "saque realizado"
+	}
+	return "Saldo insuficiente"
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito <= 0 {
+		return "Valor de depósito invalido", c.Saldo
+	}
+	c.Saldo += valorDoDeposito
+	return "Depósito realizado com sucesso!", c.Saldo
+}
+
+// conta destino também precisa ser explicitado o uso do ponteiro
+func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorTransferencia > c.Saldo && valorTransferencia <= 0 {
+		return false
+	}
+	c.Saldo -= valorTransferencia
+	contaDestino.Depositar(valorTransferencia)
+	return true
+}
--- main.go
+++ main.go
@@ -1,53 +1,22 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
+import (
+	"banco/contas"
+	"fmt"
+)
 
 func main() {
-	contaA := ContaCorrente{}
-	contaA.titular = "silvia"
-	contaA.saldo = 500
+	contaA := contas.ContaCorrente{}
+	contaA.Titular = "silvia"
+	contaA.Saldo = 500
 
-	contaB := ContaCorrente{
-		titular: "carlos",
-		saldo:   400,
+	contaB := contas.ContaCorrente{
+		Titular: "carlos",
+		Saldo:   400,
 	}
 
 	status := contaA.Transferir(300, &contaB)
 	fmt.Println(status)
 	status = contaA.Transferir(300, &contaB)
 	fmt.Println(status)
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque <= c.saldo && valorDoSaque > 0
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "saque realizado"
-	}
-	return "saldo insuficiente"
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito <= 0 {
-		return "Valor de depósito invalido", c.saldo
-	}
-	c.saldo += valorDoDeposito
-	return "Depósito realizado com sucesso!", c.saldo
-}
-
-// conta destino também precisa ser explicitado o uso do ponteiro
-func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorTransferencia > c.saldo && valorTransferencia <= 0 {
-		return false
-	}
-	c.saldo -= valorTransferencia
-	contaDestino.Depositar(valorTransferencia)
-	return true
 }
